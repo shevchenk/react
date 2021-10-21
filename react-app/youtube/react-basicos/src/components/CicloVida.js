c6ee0a3ef727dd1945c8c9a92f14f536ccc60827
shevchenk <jorgeshevchenk@gmail.com>
2021-10-21 09:05:47 -0500

    Ciclo de vida de REACT

--- react-app/youtube/react-basicos/src/components/CicloVida.js
+++ react-app/youtube/react-basicos/src/components/CicloVida.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 
 class Reloj extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount = () =>{
+        console.log('1: El componente ya se encuentra en el DOM')
+    }
+
     render(){
         return(
-            <h3>Reloj</h3>
+            <h3>Reloj Hora: {this.props.hora}</h3>
         )
     }
 }
@@ -13,25 +21,26 @@ export default class CicloVida extends Component{
     temporizador = null 
 
     state = {
-        hora: new Date().toLocaleTimeString()
+        hora: new Date().toLocaleTimeString(),
+        visible: false
     }
     
     //console.log('0: Inicializa antes del DOM')
     Iniciar = () => {
         this.temporizador = setInterval( () => {
             this.setState({
-                hora: new Date().toLocaleTimeString()
+                hora: new Date().toLocaleTimeString(),
+                visible: true
             })
         }, 1000)
     }
 
     Detener = () => {
         clearInterval(this.temporizador)
-    }
-
-    componentDidMount = () =>{
-        console.log('1: El componente ya se encuentra en el DOM')
-    }
+        this.setState({
+            visible: false
+        })
+    }    
 
     componentDidUpdate = (prevProps, prevState ) =>{
         console.log('2: El estado o las props se ha actualizado')
@@ -48,7 +57,7 @@ export default class CicloVida extends Component{
         return(
         <div>
             <h1>Ciclo Vida de los componentes de Clase</h1>
-            <h3>Hora: {this.state.hora}</h3>
+            <h3>{ this.state.visible ? <Reloj hora={this.state.hora}></Reloj> : '' }</h3>
             <button onClick={this.Iniciar}>Iniciar</button>
             <button onClick={this.Detener}>Detener</button>
         </div>
